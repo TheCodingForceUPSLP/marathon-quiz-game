b0a25f3041ca899f995e07438a3e84f19ad9875f
G. Franco Delgado <gfdgeras@gmail.com>
2024-11-06 15:08:42 -0600

    Add question structure and function prototypes

---
+++ marathon_quiz.cpp
@@ -0,0 +1,14 @@
+#define MAX_STRING 256
+
+typedef struct Question {
+    char question[MAX_STRING];
+    char options[3][MAX_STRING];
+    int correct_answer;
+    struct Question* next;
+} Question;
+
+// Function prototypes
+Question* createQuestion();
+void addQuestion(Question** head);
+void playGame(Question* head);
+void freeQuestions(Question* head);
