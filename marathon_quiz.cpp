ea1e32e7f6e319f1cd242e99bdb8eb0c0c4fc1a9
Hector <185003@upslp.edu.mx>
2024-11-18 13:30:39 -0600

    add new menu option

--- marathon_quiz.cpp
+++ marathon_quiz.cpp
@@ -32,13 +32,16 @@ int main(){
                 playGame(head);
                 break;
             case 3:
+            	printf("Ranking Players\n");
+            	break;
+            case 4:
                 printf("\n============================\n");
                 printf("    PROGRAM CREDITS\n");
                 printf("============================\n");
                 printf("Marathon Quiz Game\n");
                 printf("Developed by E13A Group\n\n");
                 break;
-            case 4:
+            case 5:
                 printf("Bye bye ...\n");
                 freeQuestions(head);
                 return 0;
@@ -59,8 +62,9 @@ void showMenu(int *choice){
     printf("============================\n");
     printf("1. Register new question\n");
     printf("2. Play game\n");
-    printf("3. Show credits\n");
-    printf("4. Exit\n");
+    printf("3. Show Ranking\n");
+    printf("4. Show credits\n");
+    printf("5. Exit\n");
 
     printf("Select an option: ");
     scanf("%d", choice);
